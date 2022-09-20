3ae843ece3344736bee0114e55c9ee84fbcc9cd2
Dallysson Oliveira da Silva <dallysson.silva@outlook.com>
2022-09-20 18:57:01 -0300

    Aula 11

---
+++ moduloD/ex008.js
@@ -0,0 +1,9 @@
+var vel = 80
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+
+if(vel > 60){ //condição simples
+    console.log('Você ultrapassou a velicidade permitida. MULTADO!')
+}
+
+
+console.log('Dir sempre usando cinto de segurança!')
